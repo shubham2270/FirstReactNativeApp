1f43b9f00ea46d2395ce3d2673057bf4b75654ea
Shubham Kumar <shubham2270@gmail.com>
2020-01-18 17:17:40 +0530

    Add input and button to update name and age

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState({ name: "John Doe", age: "30" });
+  const [name, setName] = useState("John");
+  const [age, setAge] = useState("50");
+
+  const nameHandler = val => {
+    setName(val);
+  };
+
+  const ageHandler = val => {
+    setAge(val);
+  };
+
+  const setDetails = () => {
+    setData({ name, age });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>HELLO WORLD!</Text>
+      <Text>
+        My Name is {data.name} and my age is {data.age}
+      </Text>
+      <Text style={styles.title}>Enter Name:</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder="e.g: John Doe"
+        onChangeText={val => nameHandler(val)}
+      />
+      <Text style={styles.title}>Enter age:</Text>
+      <TextInput
+        style={styles.inputField}
+        keyboardType="number-pad"
+        placeholder="e.g: 78"
+        onChangeText={val => ageHandler(val)}
+      />
+      <View style={styles.button}>
+        <Button title="Submit" onPress={setDetails} />
+      </View>
     </View>
   );
 }
@@ -12,8 +46,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  title: {
+    marginTop: 10
+  },
+  inputField: {
+    borderColor: "grey",
+    borderWidth: 1,
+    width: "50%",
+    marginTop: 5,
+    borderRadius: 3,
+    padding: 5
+  },
+  button: {
+    marginTop: 10,
+    width: 100
+  }
 });
